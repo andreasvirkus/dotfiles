7ef3671d14085ade642de254e4fbc85a3cced81f
ajv <andreasvirkus@gmail.com>
2018-07-09 14:19:24 +0300

    Add VSCode settings

---
+++ vscode/settings.js
@@ -0,0 +1,39 @@
+{
+    "workbench.colorTheme": "Material Theme Darker",
+    "editor.fontSize": 16,
+    "typescript.check.npmIsInstalled": false,
+    "files.autoSave": "onFocusChange",
+    "atomKeymap.promptV3Features": true,
+    "vim.disableAnnoyingNeovimMessage": true,
+    "window.restoreWindows": "all",
+    "editor.snippetSuggestions": "top",
+    "extensions.ignoreRecommendations": true,
+    "editor.tabSize": 2,
+    "editor.insertSpaces": true,
+    "editor.detectIndentation": false,
+    "window.zoomLevel": 1,
+    "files.trimTrailingWhitespace": true,
+    "editor.cursorBlinking": "solid",
+    "explorer.confirmDragAndDrop": false,
+    "explorer.confirmDelete": false,
+    "editor.fontFamily": "Fira Code",
+    "editor.fontLigatures": true,
+    "materialTheme.cache.workbench.settings": {
+      "themeColours": "Palenight"
+    },
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.wordWrap": "bounded",
+    "workbench.activityBar.visible": false,
+    "workbench.startupEditor": "newUntitledFile",
+    "materialTheme.fixIconsRunning": false,
+    "workbench.iconTheme": "eq-material-theme-icons-darker",
+    "eslint.validate": [
+        "javascript",
+        "javascriptreact",
+        { "language": "vue", "autoFix": true },
+        { "language": "html", "autoFix": true }
+    ],
+    "workbench.colorCustomizations": {},
+    "materialTheme.accentPrevious": "",
+    "prettier.eslintIntegration": true
+  }
