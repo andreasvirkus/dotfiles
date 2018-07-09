20ff93f5d7bc762496d5b0a0076e4cc59f268727
ajv <andreasvirkus@gmail.com>
2018-07-09 14:18:42 +0300

    Add VSCode extensions list

---
+++ vscode/extensions.sh
@@ -0,0 +1,29 @@
+# Use this to list all extensions and create install commands
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+# Latest list of extensions to install:
+code --install-extension Zignd.html-css-class-completion
+code --install-extension azemoh.one-monokai
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dariofuzinato.vue-peek
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension esbenp.prettier-vscode
+code --install-extension fknop.vscode-npm
+code --install-extension formulahendry.terminal
+code --install-extension gerane.Theme-PastelsonDark
+code --install-extension gerane.Theme-peel
+code --install-extension gerane.Theme-TomorrowNight
+code --install-extension gerane.Theme-TwoStones
+code --install-extension jamesmaj.easy-icons
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension octref.vetur
+code --install-extension sleistner.vscode-fileutils
+code --install-extension vangware.dark-plus-material
+code --install-extension vscodevim.vim
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension wix.vscode-import-cost
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension zhuangtongfa.Material-theme
