1e281a5f1154a18f479d1b1b1579d7c62ef657f2
antc <andreasvirkus>
2018-07-18 09:56:29 +0300

    Add third movements to slate config

--- slate/.slate.js
+++ slate/.slate.js
@@ -2,203 +2,168 @@
  * This config adds simple push operations to split the window
  * left/right/top/bottom or fullscreen it whilst keeping a
  * predefined margin around the windows.
- * 
+ *
  * Also allows to throw windows to the prev/next screen.
  */
 
-var margin = 15;
+const margin = 15
 
 // Create Operations
-var pushRight = S.operation("move", {
-  "x" : "screenOriginX+screenSizeX/2+" + margin / 2,
-  "y" : "screenOriginY+" + margin,
-  "width" : "screenSizeX/2-" + margin * 1.5,
-  "height" : "screenSizeY-" + margin * 2
+const pushRight = S.operation('move', {
+  'x' : 'screenOriginX+screenSizeX/2+' + margin / 2,
+  'y' : 'screenOriginY+' + margin,
+  'width' : 'screenSizeX/2-' + margin * 1.5,
+  'height' : 'screenSizeY-' + margin * 2
+})
+const pushRightThird = S.operation('move', {
+  'x' : 'screenOriginX+screenSizeX/3+' + margin / 2,
+  'y' : 'screenOriginY+' + margin,
+  'width' : 'screenSizeX/3-' + margin * 1.5,
+  'height' : 'screenSizeY-' + margin * 2
+})
+const pushRightTwoThirds = S.operation('move', {
+  'x' : 'screenOriginX+screenSizeX/3*2+' + margin / 2,
+  'y' : 'screenOriginY+' + margin,
+  'width' : 'screenSizeX/3-' + margin * 1.5,
+  'height' : 'screenSizeY-' + margin * 2
+})
+
+const pushLeft = S.operation('move', {
+  'x' : 'screenOriginX+' + margin,
+  'y' : 'screenOriginY+' + margin,
+  'width' : 'screenSizeX/2-' + margin * 1.5,
+  'height' : 'screenSizeY-' + margin * 2
+})
+const pushLeftThird = S.operation('move', {
+  'x' : 'screenOriginX+' + margin,
+  'y' : 'screenOriginY+' + margin,
+  'width' : 'screenSizeX/3-' + margin * 1.5,
+  'height' : 'screenSizeY-' + margin * 2
+})
+
+const pushTop = S.operation('move', {
+  'x' : 'screenOriginX+' + margin,
+  'y' : 'screenOriginY+' + margin,
+  'width' : 'screenSizeX-' + margin * 2,
+  'height' : 'screenSizeY/2-' + margin * 1.5
+})
+
+const pushBottom = S.operation('move', {
+  'x' : 'screenOriginX+' + margin,
+  'y' : 'screenOriginY+screenSizeY/2+' + margin / 2,
+  'width' : 'screenSizeX-' + margin * 2,
+  'height' : 'screenSizeY/2-' + margin * 1.5
+})
+
+const fullscreen = S.operation('move', {
+  'x' : 'screenOriginX+' + margin,
+  'y' : 'screenOriginY+' + margin,
+  'width' : 'screenSizeX-' + margin * 2,
+  'height' : 'screenSizeY-' + margin * 2
+})
+
+const centered = S.operation('move', {
+  'x': 'screenOriginX+150',
+  'y': 'screenOriginY+80',
+  'width': 'screenSizeX-300',
+  'height': 'screenSizeY-180'
+})
+const centeredThird = S.operation('move', {
+  'x' : 'screenOriginX+screenSizeX/3+' + margin / 2,
+  'y' : 'screenOriginY+' + margin,
+  'width' : 'screenSizeX/3-' + margin * 1.5,
+  'height' : 'screenSizeY-' + margin * 2
+})
+
+const throwNextLeft = slate.operation('throw', {
+  'width': 'screenSizeX/2-' + margin * 1.5,
+  'height': 'screenSizeY-' + margin * 2,
+  'screen': 'next'
+})
+
+const throwNextRight = slate.operation('throw', {
+  'x': 'screenOriginX+(screenSizeX)/2+' + margin,
+  'y': 'screenOriginY+' + margin,
+  'width': 'screenSizeX/2+' + margin * 1.5,
+  'height': 'screenSizeY+' + margin * 2,
+  'screen': 'next'
+})
+
+const throwNextFullscreen = slate.operation('throw', {
+  'x': 'screenOriginX',
+  'y': 'screenOriginY',
+  'width': 'screenSizeX',
+  'height': 'screenSizeY',
+  'screen': 'next'
 });
 
-var pushLeft = S.operation("move", {
-  "x" : "screenOriginX+" + margin,
-  "y" : "screenOriginY+" + margin,
-  "width" : "screenSizeX/2-" + margin * 1.5,
-  "height" : "screenSizeY-" + margin * 2
-});
+const throwNext = function(win) {
+  if (!win) return
+  const winRect = win.rect()
+  const screen = win.screen().visibleRect()
 
-var pushTop = S.operation("move", {
-  "x" : "screenOriginX+" + margin,
-  "y" : "screenOriginY+" + margin,
-  "width" : "screenSizeX-" + margin * 2,
-  "height" : "screenSizeY/2-" + margin * 1.5
-});
+  const newX = (winRect.x - screen.x)/screen.width+'*screenSizeX+screenOriginX'
+  const newY = (winRect.y - screen.y)/screen.height+'*screenSizeY+screenOriginY'
+  const newWidth = winRect.width/screen.width+'*screenSizeX'
+  const newHeight = winRect.height/screen.height+'*screenSizeY'
+  const throwNext = slate.operation('throw', {
+    'x': newX,
+    'y': newY,
+    'width': newWidth,
+    'height': newHeight,
+    'screen': 'next'
+  })
+  win.doOperation(throwNext)
+}
 
-var pushBottom = S.operation("move", {
-  "x" : "screenOriginX+" + margin,
-  "y" : "screenOriginY+screenSizeY/2+" + margin / 2,
-  "width" : "screenSizeX-" + margin * 2,
-  "height" : "screenSizeY/2-" + margin * 1.5
-});
+const pushedLeft = function(win) {
+  if (!win) return false
+  const winRect = win.rect()
+  const screen = win.screen().visibleRect()
 
-var fullscreen = S.operation("move", {
-  "x" : "screenOriginX+" + margin,
-  "y" : "screenOriginY+" + margin,
-  "width" : "screenSizeX-" + margin * 2,
-  "height" : "screenSizeY-" + margin * 2
-});
+  return winRect.x === screen.x
+    && winRect.y === screen.y
+    && winRect.width === screen.width / 2
+    && winRect.height === screen.height
+}
 
-var centered = S.operation("move", {
-  "x": "screenOriginX+150",
-  "y": "screenOriginY+80",
-  "width": "screenSizeX-300",
-  "height": "screenSizeY-180"
-});
+const pushedRight = function(win) {
+  if (!win) return false
+  const winRect = win.rect()
+  const screen = win.screen().visibleRect()
 
-var throwNextLeft = slate.operation("throw", {
-  "width": "screenSizeX/2-" + margin * 1.5,
-  "height": "screenSizeY-" + margin * 2,
-  "screen": "next"
-});
+  return winRect.x === screen.x + screen.width / 2
+    && winRect.y === screen.y
+    && winRect.width === screen.width / 2
+    && winRect.height === screen.height
+}
 
-var throwNextRight = slate.operation("throw", {
-  "x": "screenOriginX+(screenSizeX)/2+" + margin,
-  "y": "screenOriginY+" + margin,
-  "width": "screenSizeX/2+" + margin * 1.5,
-  "height": "screenSizeY+" + margin * 2,
-  "screen": "next"
-});
-
-var throwNextFullscreen = slate.operation("throw", {
-  "x": "screenOriginX",
-  "y": "screenOriginY",
-  "width": "screenSizeX",
-  "height": "screenSizeY",
-  "screen": "next"
-});
-
-var throwNext = function(win) {
-  if (!win) {
-    return;
-  }
-  var winRect = win.rect();
-  var screen = win.screen().visibleRect();
-
-  var newX = (winRect.x - screen.x)/screen.width+"*screenSizeX+screenOriginX";
-  var newY = (winRect.y - screen.y)/screen.height+"*screenSizeY+screenOriginY";
-  var newWidth = winRect.width/screen.width+"*screenSizeX";
-  var newHeight = winRect.height/screen.height+"*screenSizeY";
-  var throwNext = slate.operation("throw", {
-    "x": newX,
-    "y": newY,
-    "width": newWidth,
-    "height": newHeight,
-    "screen": "next"
-  });
-  win.doOperation(throwNext);
-};
-
-var pushedLeft = function(win) {
-  if (!win) {
-    return false;
-  }
-  var winRect = win.rect();
-  var screen = win.screen().visibleRect();
-
-  if (winRect.x === screen.x &&
-      winRect.y === screen.y &&
-      winRect.width === screen.width/2 &&
-      winRect.height === screen.height
-    ) {
-    return true;
-  }
-  return false;
-};
-
-var pushedRight = function(win) {
-  if (!win) {
-    return false;
-  }
-  var winRect = win.rect();
-  var screen = win.screen().visibleRect();
-
-  if (winRect.x === screen.x + screen.width/2 &&
-      winRect.y === screen.y &&
-      winRect.width === screen.width/2 &&
-      winRect.height === screen.height
-    ) {
-    return true;
-  }
-  return false;
-};
-
-var isFullscreen = function(win) {
-  if (!win) {
-    return false;
-  }
-  var winRect = win.rect();
-  var screen = win.screen().visibleRect();
-  if (winRect.width === screen.width &&
-      winRect.height === screen.height
-    ) {
-    return true;
-  }
-  return false;
-};
+const isFullscreen = function(win) {
+  if (!win) return false
+  const winRect = win.rect()
+  const screen = win.screen().visibleRect()
+  return winRect.width === screen.width && winRect.height === screen.height
+}
 
 
-slate.bind("left:ctrl,cmd", function(win) {
-  if (!win) {
-    return;
-  }
-  if (pushedLeft(win)) {
-    win.doOperation(throwNextLeft);
-  } else {
-    win.doOperation(pushLeft);
-  }
-});
+slate.bind('left:ctrl,cmd', function(win) {
+  if (!win) return
 
-slate.bind("right:ctrl,cmd", function(win) {
-  if (!win) {
-    return;
-  }
+  win.doOperation(pushLeftThird);
+})
 
-  if (pushedRight(win)) {
-    win.doOperation(throwNextRight);
-  } else {
-    win.doOperation(pushRight);
-  }
-});
+slate.bind('right:ctrl,cmd', function(win) {
+  if (!win) return
 
-slate.bind("up:ctrl,cmd", function(win) {
-  if (!win) {
-    return;
-  }
+  win.doOperation(pushRightTwoThirds);
+})
 
-  if (isFullscreen(win)) {
-    win.doOperation(throwNextFullscreen);
-  } else {
-    win.doOperation(fullscreen);
-  }
-});
-
-slate.bind("down:ctrl,cmd", function(win) {
-  if (!win) {
-    return;
-  }
-
-  if (pushedLeft(win)) {
-    win.doOperation(throwNextLeft);
-  } else if (pushedRight(win)) {
-    win.doOperation(throwNextRight);
-  } else if (isFullscreen(win)) {
-    win.doOperation(throwNextFullscreen);
-  } else {
-    throwNext(win);
-  }
-});
-
-S.bind("right:shift;alt;cmd", throwNextRight);
-S.bind("left:shift;alt;cmd", throwNextLeft);
-S.bind("left:ctrl;alt;cmd", pushLeft);
-S.bind("right:ctrl;alt;cmd", pushRight);
-S.bind("down:ctrl;alt;cmd", pushBottom);
-S.bind("up:ctrl;alt;cmd", pushTop);
-S.bind("c:ctrl;alt;cmd", centered);
-S.bind("m:ctrl;alt;cmd", fullscreen);
+S.bind('right:shift;alt;cmd', throwNextRight)
+S.bind('left:shift;alt;cmd', throwNextLeft)
+S.bind('left:ctrl;alt;cmd', pushLeft)
+S.bind('right:ctrl;alt;cmd', pushRight)
+S.bind('down:ctrl;alt;cmd', pushBottom)
+S.bind('up:ctrl;alt;cmd', pushTop)
+S.bind('c:ctrl;alt;cmd', centered)
+S.bind('c:ctrl;cmd', centeredThird)
+S.bind('m:ctrl;alt;cmd', fullscreen)
