0dd1875548dd3abe8b2bb902f6029b8d5d7bcf55
andreas <andreasvirkus@gmail.com>
2020-04-13 11:55:00 +0300

    Update VSCode extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -2,14 +2,15 @@
 # code --list-extensions | xargs -L 1 echo code --install-extension
 
 # Latest list of extensions to install:
-code --install-extension Zignd.html-css-class-completion
 code --install-extension azemoh.one-monokai
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension christian-kohler.path-intellisense
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension dariofuzinato.vue-peek
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension Equinusocio.vsc-community-material-theme
 code --install-extension Equinusocio.vsc-material-theme
+code --install-extension equinusocio.vsc-material-theme-icons
 code --install-extension esbenp.prettier-vscode
 code --install-extension fknop.vscode-npm
 code --install-extension formulahendry.terminal
@@ -17,12 +18,16 @@ code --install-extension gerane.Theme-PastelsonDark
 code --install-extension gerane.Theme-peel
 code --install-extension gerane.Theme-TomorrowNight
 code --install-extension gerane.Theme-TwoStones
+code --install-extension JamesBirtles.svelte-vscode
 code --install-extension jamesmaj.easy-icons
+code --install-extension ms-vscode.Go
 code --install-extension octref.vetur
+code --install-extension pflannery.vscode-versionlens
 code --install-extension sleistner.vscode-fileutils
 code --install-extension vangware.dark-plus-material
 code --install-extension vscodevim.vim
 code --install-extension wayou.vscode-todo-highlight
 code --install-extension wix.vscode-import-cost
 code --install-extension yzhang.markdown-all-in-one
-code --install-extension zhuangtongfa.Material-theme
+code --install-extension zhuangtongfa.material-theme
+code --install-extension Zignd.html-css-class-completion
